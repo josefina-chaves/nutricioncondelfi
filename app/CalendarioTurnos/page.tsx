925f14cd903875871dc8672a8d5484e1c3c9f662
Josefina Chaves <joochaves05@gmail.com>
2026-08-27 09:41:49 -0300

    Turnos estandarizados a 1 hora en codigo completo

--- app/CalendarioTurnos/page.tsx
+++ app/CalendarioTurnos/page.tsx
@@ -77,8 +77,8 @@ const DISPONIBILIDAD = {
   },
   2: { 
     'virtual': ["08:00", "09:00", "10:00", "11:00", "12:00"],
-    'presencial-antropometria': ["14:30", "15:30", "16:30"],
-    'presencial-seguimiento': ["14:30", "15:30", "16:30"],
+    'presencial-antropometria': ["14:30", "15:30", "16:30", "17:30"],
+    'presencial-seguimiento': ["14:30", "15:30", "16:30", "17:30"],
   },
   3: { 
     'virtual': ["08:00", "09:00", "10:00", "11:00", "16:00", "17:00", "18:00", "19:00"],
